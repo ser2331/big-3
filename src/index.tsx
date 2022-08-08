5226436c2a882d32280fb40ab9a5f50aa7d67898
laptop <ser2331@gmail.com>
2022-08-08 22:09:04 +0300

    upstream

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { setupStore } from "./components/core/redux/store";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import App from "./components/app";
 
