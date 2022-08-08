3a47fb841923772214559db3f7f45cc70a29eba2
laptop <ser2331@gmail.com>
2022-08-08 20:21:47 +0300

    heroku

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { setupStore } from "./components/core/redux/store";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./components/app";
 
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <Provider store={store}>
-        <Router >
+        <Router>
             <App />
         </Router>
     </Provider>
