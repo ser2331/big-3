a771bcd3b77e696de2a034fb8fa575097c7eb6d9
laptop <ser2331@gmail.com>
2022-08-24 13:33:14 +0300

    add error message then isUnauthorized

--- src/components/modules/authorization/components/AuthContent/Login.tsx
+++ src/components/modules/authorization/components/AuthContent/Login.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useEffect} from "react";
+import React, {FC, useEffect, useMemo} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup/dist/yup";
@@ -11,6 +11,7 @@ import { Field } from "../../../../common/components/field/field";
 import { CustomButton } from "../../../../common/components/custom-button/custom-button";
 import { IGetToken } from "../../interfaces/authorization-interfaces";
 import Types from "../../../../types";
+import { ErrorMessage } from "../../../../common/components/error-message/error-message";
 
 import s from "./AuthWrapper.module.scss";
 
@@ -21,10 +22,14 @@ const { setUserData, setSignOut } = authorizationSlice.actions;
 const Login: FC = () => {
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
-    const [getToken, {data, isError}] = authService.useGetSignInTokenMutation();
+    const [getToken, {data, isError, error: tokenError}] = authService.useGetSignInTokenMutation();
 
     const formOptions = { resolver: yupResolver(loginSchema) };
     const {register, handleSubmit, formState: { errors }} = useForm<IGetToken>(formOptions);
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const isUnauthorized = useMemo(() => { return tokenError?.status === 401; }, [tokenError]);
 
     useEffect(() => {
         if (data && !isError) {
@@ -46,6 +51,7 @@ const Login: FC = () => {
 
     return (
         <div className={`${s.Login} ${s.AuthWrapper}`}>
+            {isUnauthorized && <ErrorMessage message="Пользователь с таким логином/поролем не найден" />}
             <h1 className="auth-title">Sign In</h1>
             <form onSubmit={handleSubmit(submit)}>
                 <Field
@@ -56,7 +62,7 @@ const Login: FC = () => {
                 />
 
                 <Field
-                    label="Name"
+                    label="Password"
                     register={register}
                     registerName="password"
                     error={errors.password}
